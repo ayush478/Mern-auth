78501a6ba0e22319d5755f56a25f4fdda84e501f
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-08-13 21:25:19 +0530

    Routing

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import useStyles from './styles';
+const Auth = () => {
+  const state = null;
+  return <div>AUth</div>;
+};
+
+export default Auth;
