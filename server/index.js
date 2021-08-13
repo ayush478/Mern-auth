d2b2c1cd902c40b0671c2c69a16d525733bad2e3
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-08-14 03:17:11 +0530

    Deployment on heroku and netlify

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
+dotenv.config()
 
 const app = express();
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
